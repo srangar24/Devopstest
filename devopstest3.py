f671eefc8fb69f000c3d99f0d58ed0e8e6656fc9
Shalini <srangar09@gmail.com>
2024-11-07 21:52:34 -0500

    Created demotest3.py

---
+++ devopstest3.py
@@ -0,0 +1,11 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        return f"{self.name} says woof!"
+
+dog = Dog("Buddy", 3)
+print(dog.bark())
+
